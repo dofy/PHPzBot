4194f0d9742cac3b8530d8e7f1c87b8ca4ada2e3
Seven Yu <dofyyu@gmail.com>
2017-09-27 16:45:23 +0800

    parse text message

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const route = `/phpzbot${config.token}`;
 
 const bot = new Telegraf(config.token);
 
+bot.catch(err => {
+  console.error(err);
+})
+
 bot.use((ctx, next) => {
   console.log(ctx.message);
-  next(ctx);
+  return next(ctx);
+});
+
+bot.on('text', {reply, me, message, from, chat} => {
+  console.log(me, message, from, chat);
+  reply(`>>> ${chat.type}:${chat.username || chat.title}`);
 });
 
 bot.startWebhook(route, null, config.port);
-
-bot.on('text', ctx => {
-  ctx.reply(`>>> ${ctx.message.chat.type}:${ctx.message.chat.username}`);
-});
