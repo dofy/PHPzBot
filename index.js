d500ee6a15999f6c227f9e0ae86a7450f61972ee
Seven Yu <dofyyu@gmail.com>
2017-09-27 15:00:39 +0800

    add debug middleware

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const route = `/phpzbot${config.token}`;
 
 const bot = new Telegraf(config.token);
 
+bot.use((ctx, next) => {
+  console.log(ctx.message);
+  next(ctx);
+});
+
 bot.startWebhook(route, null, config.port);
 
 bot.on('text', ctx => {
-  ctx.reply(`>>> ${ctx.message.chat.id}`);
+  ctx.reply(`>>> ${ctx.message.chat.type}:${ctx.message.chat.username}`);
 });
